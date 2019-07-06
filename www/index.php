2e01717d131e9fdaa5a4d39384fcbaa17a202fce
bykorozec <bykorozec@gmail.com>
2019-07-06 16:02:07 +0200

    plik index.php

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <title>Przykład</title>
+  </head>
+  <body>
+<?php
+echo "Hello world!<br/>";
+phpinfo();
+?>  
+  </body>
+</html>
